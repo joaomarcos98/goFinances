41bee16e7d28571254aec110fc0b8d5f2abd9349
Joao Marcos <joomarcos.belanga98@gmail.com>
2022-03-28 14:36:18 -0300

    add: theme

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Dashboard } from './screens/Dashboard';
+
+export default function App() {
+
+    return <Dashboard />
+}
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./styles"
+
+export const Dashboard = () => {
+    return (
+        <S.Container >
+            <S.Title >Alo alonso</S.Title>
+        </S.Container>
+    )
+}
