f09f5003eb4783e58101cda28ff0849853241051
Joao Marcos <joomarcos.belanga98@gmail.com>
2022-03-29 14:51:49 -0300

    add: create transaction type component for register

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "../../components/Forms/Input";
-import * as S from "./styles"
+import { Button } from "../../components/Forms/Button";
+import { TransactionTypeButton } from "../../components/Forms/TransactionTypeButton";
+import * as S from "./styles";
+
+
+type transactionTypeProps = "up" | "down" | ""
 
 export const Register = () => {
 
+    const [transactionType, setTransactionType] = useState<transactionTypeProps>("");
+
+    const handleTransactionTypeSelect = (type: "up" | "down") => {
+        setTransactionType(type)
+    }
 
     return (
         <S.Container>
@@ -12,13 +22,29 @@ export const Register = () => {
             </S.Header>
 
             <S.Form>
-                <Input
-                    placeholder="Nome"
-                />
-
-                <Input
-                    placeholder="Preço"
-                />
+                <S.Fields>
+                    <Input
+                        placeholder="Nome"
+                    />
+                    <Input
+                        placeholder="Preço"
+                    />
+                    <S.TransactionTypes>
+                        <TransactionTypeButton
+                            type="up"
+                            title="Income"
+                            isActive={transactionType === "up"}
+                            onPress={() => handleTransactionTypeSelect("up")}
+                        />
+                        <TransactionTypeButton
+                            type="down"
+                            title="Outcome"
+                            isActive={transactionType === "down"}
+                            onPress={() => handleTransactionTypeSelect("down")}
+                        />
+                    </S.TransactionTypes>
+                </S.Fields>
+                <Button title="Enviar" />
             </S.Form>
         </S.Container>
     )
